4fa96e5fb2bb106aa3cf3f3c7f0a8b4c6e24275b
Le Duong <leduongnewmemes123@gmail.com>
2025-08-14 16:35:33 +0700

    feat: Finished Form's HTML implentation

--- src/components/CvForms.jsx
+++ src/components/CvForms.jsx
@@ -140,8 +140,8 @@ function EducationFormBox({ education, setSelectedId, selectedId, setEducationLi
                         />
                     </p>       
                     <p>
-                        <label htmlFor="date"></label>
-                        <input type="text" name="date" id="date"
+                        <label htmlFor="date-education"></label>
+                        <input type="text" name="date-education" id="date-education"
                             value={education.date}
                             onChange={(e) => {
                                 handleEdit(e, 'date')}
@@ -167,4 +167,147 @@ function EducationFormBox({ education, setSelectedId, selectedId, setEducationLi
             </>
         )
     }  
+}
+
+export function ExperienceForm({ listOfExperience, setExperienceList }) {
+    let [selectedId, setSelectedId] = useState(null)
+
+    function handleAddExperience() {
+        // create new experience object
+        // return a new experienceList with that object
+        let newExperience = {
+            id: crypto.randomUUID(),
+            job: 'Your Job',
+            position: 'Your Position',
+            description: 'Your Experience Description (eg: contributed to something, increased revenue or something like that)',
+            date: 'Start Date - End Date'
+        }
+        setExperienceList([...listOfExperience, newExperience])
+    }
+    
+    return (
+        <>
+            <h2>Experience</h2>
+            <hr />
+            <div className="experience-form-box-container">
+                {listOfExperience.map((item) => {
+                    return <ExperienceBox experience={item}
+                                          selectedId={selectedId} 
+                                          listOfExperience={listOfExperience}
+                                          setSelectedId={setSelectedId}
+                                          setExperienceList={setExperienceList}
+                                          />
+                })}
+            </div>
+            <button onClick={handleAddExperience} type="button">Add Experience</button>
+        </>
+    )
+}
+
+function ExperienceBox({ experience, selectedId, listOfExperience, setSelectedId, setExperienceList }) {
+    function handleSelect() {
+        // set selectedId to current education.id
+        setSelectedId(education.id)
+    }
+
+    function handleRemove() {
+        // remove the object with the current education.id
+        // return a new educationList without that object
+        
+        let listCopy = listOfEducations.map((object) => {
+            return {...object}
+        })  
+        let remainingList = listCopy.filter((object) => {
+            if (object.id !== education.id) return true
+            else return false
+        })
+        setEducationList(remainingList)
+    }
+
+    function handleEdit(e, field) {
+        // select object with selectedId in listOfEducation
+        // change that object field
+        // return a new educationList with that object
+
+        let changedList = listOfEducations.map((object) => {
+            if (object.id === education.id) {
+                return {...object, [field]: e.target.value}
+            } else {
+                return {...object}
+            }
+        })
+
+        setEducationList(changedList)
+    }
+
+    function handleClose() {
+        // set selectedId to null
+        setSelectedId(null)
+    }
+    
+    
+    if (selectedId === experience.id) {
+        return (
+            <div className="experience-box">
+                <div className="experience-header-container">
+                    <h3>{experience.job}</h3>
+                    <button onClick={handleSelect} type="button">Edit</button>
+                    <button onClick={handleRemove} type="button">Remove</button>
+                </div>
+                <div className="experience-form">
+                    <p>
+                        <label htmlFor="job"></label>
+                        <input type="text" name="job" id="job"
+                            value={experience.job}
+                            onChange={(e) => {
+                                handleEdit(e, 'job')}
+                            }
+                        />
+                    </p>    
+                    <p>
+                        <label htmlFor="position"></label>
+                        <input type="text" name="position" id="position"
+                            value={experience.position}
+                            onChange={(e) => {
+                                handleEdit(e, 'position')}
+                            }
+                        />
+                    </p>       
+                    <p>
+                        <label htmlFor="description"></label>
+                        <textarea type="text" name="description" id="description"
+                            value={experience.description}
+                            onChange={(e) => {
+                                handleEdit(e, 'description')}
+                            }
+                        />
+                    </p>
+                    <p>
+                        <label htmlFor="date-experience"></label>
+                        <input type="text" name="date-experience" id="date-experience"
+                            value={experience.date}
+                            onChange={(e) => {
+                                handleEdit(e, 'date')}
+                            }
+                        />
+                    </p>
+                </div>
+                <button onClick={handleClose} type="button">Close</button>
+                <hr />
+            </div> 
+        )
+    } else {
+        return (
+            <>
+                <div className="experience-box">
+                    <div className="experience-header-container">
+                        <h3>{experience.job}</h3>
+                        <button onClick={handleSelect} type="button">Edit</button>
+                        <button onClick={handleRemove} type="button">Remove</button>
+                    </div>
+                </div>
+                <hr />
+            </>
+        )
+    }
 }
